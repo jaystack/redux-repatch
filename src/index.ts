9013c985ca2fc194f8f96c0275404e14d82feee5
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-08-29 05:04:56 +0200

    export default repatch enhancer

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 import { StoreEnhancer, Reducer, AnyAction, StoreCreator, applyMiddleware, Middleware } from 'redux';
 import { Reducer as RepatchReducer } from 'repatch';
 
-export const REPATCH = '@@repatch/NEXT_STATE';
+const REPATCH = '@@repatch/NEXT_STATE';
 
-export const middleware: Middleware = store => next => action => {
+const middleware: Middleware = store => next => action => {
   if (typeof action !== 'function') return next(action);
   const result = action(store.getState());
   return typeof result === 'function' ? next(result) : next({ type: REPATCH, nextState: result });
 };
 
-export const extendReducer = <S>(reducer: Reducer<S>): Reducer<S> => (state: S, action: AnyAction) =>
+const extendReducer = <S>(reducer: Reducer<S>): Reducer<S> => (state: S, action: AnyAction) =>
   action.type === REPATCH ? action.nextState : reducer(state, action);
 
-export const extendEnhancer = enhancer =>
-  enhancer ? enhancer(applyMiddleware(middleware)) : applyMiddleware(middleware);
+const extendEnhancer = enhancer => (enhancer ? enhancer(applyMiddleware(middleware)) : applyMiddleware(middleware));
 
 export const repatch = createStore => (reducer, preloadedState, enhancer) =>
   createStore(extendReducer(reducer), preloadedState, extendEnhancer(enhancer));
+
+export default repatch;
